060540c82d54597101db09297a222ae397564393
Geordyn Ader <geordyn.ader@gmail.com>
2016-05-04 15:52:12 -0500

    hosting

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ var app = express();
 
 app.use(express.static(__dirname + '/../public'));
 
-app.listen(80, function(){
-  console.log('listening on port 8081');
+app.listen(8081, function(){
+  console.log('akemp.co on port 8081');
 });
